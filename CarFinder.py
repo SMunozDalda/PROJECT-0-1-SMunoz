74031844a9c867897e8a13f91cf0fa2f3179a200
SMunozDalda <cmunoz17@hawkmail.hccfl.edu>
2025-04-19 17:50:07 -0400

    Create CarFinder.py

---
+++ CarFinder.py
@@ -0,0 +1,38 @@
+AllowedVehiclesList = [
+    'Ford F-150',
+    'Chevrolet Silverado',
+    'Tesla CyberTruck',
+    'Toyota Tundra',
+    'Nissan Titan'
+]
+
+def print_menu():
+    print("********************************")
+    print("AutoCountry Vehicle Finder v0.1")
+    print("********************************")
+    print("Please Enter the following number below from the following menu:\n")
+    print("1. PRINT all Authorized Vehicles")
+    print("2. Exit")
+
+def print_vehicles():
+    print("\nThe AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
+    for vehicle in AllowedVehiclesList:
+        print(vehicle)
+    print()
+
+def main():
+    while True:
+        print_menu()
+        user_input = input()
+
+        if user_input == "1":
+            print_vehicles()
+        elif user_input == "2":
+            print("\nThank you for using the AutoCountry Vehicle Finder, good-bye!")
+            break
+        else:
+            print("\nInvalid input. Please enter 1 or 2.\n")
+
+    # Wait for keypress before closing (for Windows CMD)
+    input("\nProgram ended; press any key or close the console...")
+
